8bb4179a4229676c10342fc908c1666673129b80
ezifarel2 <ezifarel2@gmail.com>
2026-04-10 14:20:35 +0700

    terbaru

--- app/Views/login.php
+++ app/Views/login.php
@@ -2,7 +2,7 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <title>jancok | LAB-SYS</title>
+    <title>Login | LAB-SYS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
